45cd6512fe878c9039f6eff31ccf4032edb6db05
Tiago Rodrigues <isatiago098@gmail.com>
2021-11-17 20:35:47 -0300

    resoluçao exercicio SequenciaCentena

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("exibe o numero 100 veses");
+              int contador = 1;
+              while (contador <= 100)
+              {
+                   Console.WriteLine(contador);
+                   contador = contador + 1;
+              }
+            
+            Console.WriteLine(" boa noite, muito obrigado");
+
+        }
+    }
+}
